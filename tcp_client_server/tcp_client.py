c82f900c44011548657c6571826b8551ff0c7126
nmasdoufi <58260391+nmasdoufi@users.noreply.github.com>
2021-06-19 08:11:40 +0100

    Update tcp_client.py

--- tcp_client_server/tcp_client.py
+++ tcp_client_server/tcp_client.py
@@ -4,7 +4,7 @@ from socket import AF_INET, SOCK_STREAM, socket
 from threading import Thread
 
 s = socket(AF_INET, SOCK_STREAM)
-host, port = "127.0.0.1", 1337
+host, port = "127.0.0.1", 9999
 s.connect((host, port))
 
 print(s.recv(1024))
